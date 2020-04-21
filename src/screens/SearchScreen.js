c97f695fbca1b1d6f8a7a7f8513952974834cc0f
Ryley Dennis <ryleydennis@gmail.com>
2020-04-21 15:39:27 -0400

    Beginning implementation of search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+import { LinearGradient } from 'expo-linear-gradient';
 
 
-export default class SearchScreen extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Search Screen</Text>
-            </View>
-        )
-    }
+export default function SearchScreen() {
+    const [input, setInput] = useState('');
+
+    return (
+        <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
+            <LinearGradient
+                style={{ flex: 1, alignItems: 'center' }}
+                colors={['#FFFBF1', '#FFEDC0']}
+            >
+                    <TextInput
+                        style={styles.input}
+                        placeholder='New York'
+                        onChangeText={text => setInput(text)}
+                        
+                    />
+                    <View style={styles.underLine}/>
+            </LinearGradient>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    input: {
+        textAlign: 'center',
+        marginTop: 60,
+        height: 30,
+        width: 230,
+        borderRadius: 10,
+        fontSize: 20,
+        color: '#494949'
+    },
+    underLine: {
+        height: 0.75,
+        width: 250,
+        backgroundColor: '#494949',
+    }
+})
