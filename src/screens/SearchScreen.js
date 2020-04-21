4ed66a6ed78ca7ccf503344ae6577ebe31ae4e5f
Ryley Dennis <ryleydennis@gmail.com>
2020-04-21 16:24:59 -0400

    Added icon pack and finished layout of input box

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,47 @@
 import React, { Component, useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather'
+import Styles, { ViewStyle } from '../Styles'
 
 
 export default function SearchScreen() {
     const [input, setInput] = useState('');
 
     return (
-        <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
-            <LinearGradient
-                style={{ flex: 1, alignItems: 'center' }}
-                colors={['#FFFBF1', '#FFEDC0']}
-            >
-                    <TextInput
-                        style={styles.input}
-                        placeholder='New York'
-                        onChangeText={text => setInput(text)}
-                        
-                    />
-                    <View style={styles.underLine}/>
-            </LinearGradient>
-        </View>
+        <LinearGradient
+            style={{ flex: 1, alignItems: 'center', paddingTop: 60 }}
+            colors={['#FFFBF1', '#FFEDC0']}
+        >
+            <View style={ViewStyle.card}>
+                <View style={styles.inputBox}>
+                    <View style={styles.leftSpacer} />
+                    <View>
+                        <TextInput
+                            style={styles.input}
+                            placeholder='New York'
+                            onChangeText={text => setInput(text)}
+                        />
+                        <View style={styles.underLine} />
+                    </View>
+                    <TouchableOpacity>
+                        <Icon
+                            color='#494949'
+                            name="chevron-right"
+                            size={30}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </LinearGradient>
     )
 }
 
 const styles = StyleSheet.create({
     input: {
         textAlign: 'center',
-        marginTop: 60,
+        alignSelf: 'center',
         height: 30,
         width: 230,
         borderRadius: 10,
@@ -39,5 +52,15 @@ const styles = StyleSheet.create({
         height: 0.75,
         width: 250,
         backgroundColor: '#494949',
+    },
+    leftSpacer: {
+        height: 30,
+        width: 30,
+    },
+    inputBox: {
+        flexDirection: 'row',
+        margin: 6,
+        marginTop: 10,
+        marginBottom: 10,
     }
 })
